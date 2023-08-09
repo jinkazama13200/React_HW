9b3aff13083b5318a7d1356f8f600e8b6cdf8fe1
jinkazama13200 <jinkazama13200@gmail.com>
2023-08-09 18:10:51 +0700

    hw_2

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -1,6 +1,3 @@
-import Header from "./Mylayout/Header";
-import Body from "./Mylayout/Body";
-import Footer from "./Mylayout/Footer";
 import GlassesLayout from "./GlassLayout/GlassesLayout";
 
 function MyLayout() {
