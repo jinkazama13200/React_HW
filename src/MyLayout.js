bb6e5d6d45754b11dd925986f7e7ab3277974658
jinkazama13200 <jinkazama13200@gmail.com>
2023-08-09 21:07:27 +0700

    hw_1

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -1,5 +1,15 @@
+import Header from "./Mylayout/Header";
+import Body from "./Mylayout/Body";
+import Footer from "./Mylayout/Footer";
+
 function MyLayout() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
 }
 
 export default MyLayout;
---
+++ src/Mylayout/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "./Banner";
+import ListItem from "./ListItem";
+
+export default function Body() {
+  return (
+    <>
+      <Banner />
+      <ListItem />
+    </>
+  );
+}
