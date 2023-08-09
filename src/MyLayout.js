b93e08ca99238968cdfe7f829a279c3f8bc649ad
jinkazama13200 <jinkazama13200@gmail.com>
2023-08-09 19:55:25 +0700

    main branch

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -1,16 +1,5 @@
-import Header from "./Mylayout/Header";
-import Body from "./Mylayout/Body";
-import Footer from "./Mylayout/Footer";
-
 function MyLayout() {
-  return (
-    <>
-      <Header />
-      <Body />
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 
 export default MyLayout;
-
